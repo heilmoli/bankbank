5b09ed5b65ecd74250366b641b00e21ebf68b89e
heilmann <oliver@heilmann.co.uk>
2025-08-18 00:04:37 +0100

    react + some results

--- devbench.py
+++ devbench.py
@@ -7,10 +7,12 @@ import subprocess
 from collections import defaultdict
 import zipfile
 
-
+# https://github.com/facebook/react/archive/refs/tags/v19.1.1.zip
 # https://github.com/vuejs/vue/archive/refs/tags/v2.7.16.zip
-reference_archive = "./vue-2.7.16.zip"
-target_dir = "testrun"
+# reference_archive = "./vue-2.7.16.zip"
+reference_archive = "./react-19.1.1.zip"
+# target_dir = "testrun"
+target_dir = "C:\\Users\\heilmann\\testruns"
 runs=10
 
 extra_content="""
